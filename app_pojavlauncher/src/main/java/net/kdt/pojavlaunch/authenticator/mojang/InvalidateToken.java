e2c45944624ea7e71fa5e7ef3a3bc924799227bd
SerpentSpirale <serpentspirale@gmail.com>
2022-08-19 17:28:55 +0200

    New implementation of token invalidation, no async task

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/mojang/InvalidateToken.java
@@ -0,0 +1,37 @@
+package net.kdt.pojavlaunch.authenticator.mojang;
+
+import android.util.Log;
+
+import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.authenticator.mojang.yggdrasil.YggdrasilAuthenticator;
+import net.kdt.pojavlaunch.value.MinecraftAccount;
+
+import java.io.File;
+import java.util.UUID;
+
+public class InvalidateToken {
+    private final YggdrasilAuthenticator mAuthenticator = new YggdrasilAuthenticator();
+
+    public void invalidate(String filename){
+        MinecraftAccount account = MinecraftAccount.load(filename);
+        invalidate(account);
+    }
+
+    public void invalidate(MinecraftAccount account){
+        new Thread(() -> {
+            if(account == null) return;
+            new File(Tools.DIR_ACCOUNT_NEW + "/" + account.username + ".json").delete();
+
+            if(!account.accessToken.equals("0")){ // pointless to invalidate local
+                try {
+                    mAuthenticator.invalidate(account.accessToken,
+                            UUID.fromString(account.isMicrosoft ? account.profileId : account.clientToken /* should be? */));
+                } catch (Throwable e) {
+                    Log.e("InvalidateToken", e.toString());
+                }
+            }
+
+        }).start();
+    }
+
+}
