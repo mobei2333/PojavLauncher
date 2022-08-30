7b978a0e18514288121a0b4ce6b0261251786951
SerpentSpirale <serpentspirale@gmail.com>
2022-08-30 23:36:49 +0200

    Add small optimizations, fixed improper annotation

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/ProgressLayout.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/ProgressLayout.java
@@ -5,23 +5,20 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.IntegerRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.collection.ArrayMap;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.extra.ExtraCore;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 /** Class staring at specific values and automatically show something if the progress is present
@@ -30,6 +27,9 @@ import java.util.List;
  * This class relies on ExtraCore for its behavior.
  */
 public class ProgressLayout extends ConstraintLayout implements View.OnClickListener {
+    public static final String UNPACK_RUNTIME = "unpack_runtime";
+    public static final String DOWNLOAD_MINECRAFT = "download_minecraft";
+
     public ProgressLayout(@NonNull Context context) {
         super(context);
         init();
@@ -47,11 +47,10 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
         init();
     }
 
-
+    private int mActiveProcesses = 0;
     private final ArrayMap<String, TextProgressBar> mMap = new ArrayMap<>();
     private LinearLayout mLinearLayout;
     private TextView mTaskNumberDisplayer;
-    private final Handler mHandler = new Handler(Looper.getMainLooper());
     private final Runnable mCheckProgressRunnable = new Runnable() {
         @Override
         public void run() {
@@ -68,14 +67,17 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
                         TextProgressBar textView = new TextProgressBar(getContext());
                         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, getResources().getDimensionPixelOffset(R.dimen._20sdp));
                         params.bottomMargin = getResources().getDimensionPixelOffset(R.dimen._6sdp);
-                        
+
                         mLinearLayout.addView(textView, params);
                         mMap.put(progressKey, textView);
+                        mActiveProcesses++;
                     }
 
                     mMap.get(progressKey).setProgress(progress);
                     if(resourceString != -1){
-                        mMap.get(progressKey).setText(getResources().getString(resourceString, Arrays.copyOfRange(progressStuff, 2, progressStuff.length - 1)));
+                        // As an optimization, copy array content back 2 indexes instead of creating another object
+                        System.arraycopy(progressStuff, 2, progressStuff, 0, progressStuff.length - 2);
+                        mMap.get(progressKey).setText(getResources().getString(resourceString, progressStuff));
                     }else{
                         mMap.get(progressStuff[2]);
                     }
@@ -85,14 +87,15 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
                     if(progress == 0){
                         mLinearLayout.removeView(mMap.get(progressKey));
                         mMap.remove(progressKey);
+                        mActiveProcesses--;
                     }
                 }
             }
 
-            setVisibility(mMap.size() == 0 ? GONE : VISIBLE);
+            setVisibility(hasProcesses() ? VISIBLE : GONE);
 
-            mTaskNumberDisplayer.setText(mMap.keySet().size() + " tasks in progress");
-            mHandler.postDelayed(this, 1000);
+            mTaskNumberDisplayer.setText(mActiveProcesses + " tasks in progress");
+            postDelayed(this, 1000);
         }
     };
 
@@ -102,11 +105,19 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
         mMap.put(progressKey, null);
     }
 
+    public boolean hasProcesses(){
+        return mActiveProcesses > 0;
+    }
+
+    public boolean hasProcess(String process){
+        return mMap.get(process) != null;
+    }
+
     private void init(){
         inflate(getContext(), R.layout.view_progress, this);
         mLinearLayout = findViewById(R.id.progress_linear_layout);
         mTaskNumberDisplayer = findViewById(R.id.progress_textview);
-        mHandler.postDelayed(mCheckProgressRunnable, 3000);
+        postDelayed(mCheckProgressRunnable, 1000);
 
         setBackgroundColor(getResources().getColor(R.color.background_bottom_bar));
         setVisibility(GONE);
@@ -114,7 +125,7 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
         setOnClickListener(this);
     }
 
-    public static void setProgress(String progressKey, int progress, @IntegerRes int resource, String... message){
+    public static void setProgress(String progressKey, int progress, @StringRes int resource, String... message){
         StringBuilder builder = new StringBuilder();
         for(String bit : message){
             builder.append(bit).append("¤");
@@ -131,4 +142,5 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
     public void onClick(View v) {
         mLinearLayout.setVisibility(mLinearLayout.getVisibility() == GONE ? VISIBLE : GONE);
     }
+
 }
